5b379b6cc2477303035be2734ebf7209f2e11976
Water-Drop <water_drop@foxmail.com>
2015-06-14 20:36:49 +0800

    modify specific issue for the project

--- Code/src/util/MD5Helper.java
+++ Code/src/util/MD5Helper.java
@@ -3,7 +3,7 @@ package util;
 import java.security.MessageDigest;
 
 public class MD5Helper {
-    public static String generateMD5(String s) {
+    public static String encodeMD5(String s) {
         char hexDigits[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};       
         try {
             byte[] btInput = s.getBytes();
