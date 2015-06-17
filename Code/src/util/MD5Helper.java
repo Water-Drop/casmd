32c0f3eb73cea48965e12279e63a9e0536012c81
Water-Drop <water_drop@foxmail.com>
2015-06-17 23:17:26 +0800

    add getFileMD5

--- Code/src/util/MD5Helper.java
+++ Code/src/util/MD5Helper.java
@@ -1,5 +1,8 @@
 package util;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.math.BigInteger;
 import java.security.MessageDigest;
 
 public class MD5Helper {
@@ -23,5 +26,27 @@ public class MD5Helper {
             e.printStackTrace();
             return null;
         }
+    }
+    public static  String GetFileMD5(File file) {
+        if (!file.isFile()) {
+            return null;
+        }
+        MessageDigest digest = null;
+        FileInputStream in = null;
+        byte buffer[] = new byte[1024];
+        int len;
+        try {
+            digest = MessageDigest.getInstance("MD5");
+            in = new FileInputStream(file);
+            while ((len = in.read(buffer, 0, 1024)) != -1) {
+                digest.update(buffer, 0, len);
+            }
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        BigInteger bigInt = new BigInteger(1, digest.digest());
+        return bigInt.toString(16);
     }
 }
